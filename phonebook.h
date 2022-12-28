dbfbe25643537b10a37fa05e7552b1acaed70de5
Jason <119556636+Jason-lillnet@users.noreply.github.com>
2022-12-28 10:38:12 +0900

    phonebook.h 파일 추가
    phonebook.h를 추가하고,
    showlist.c, test_showlist.c 파일에 phonebook.h 를 include 함

---
+++ phonebook.h
@@ -0,0 +1,7 @@
+#ifndef __Phone_Book__
+#define __Phone_Book__
+
+extern int ShowList(stList* pList);  // 정렬된 전체 리스트를 보여주는 함수
+extern int SortList(stList* pList);  // 리스트를 정렬하는 함수 (현재는 이름 오름차순)
+
+#endif 
